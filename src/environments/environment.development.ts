fd5acd257189be5d2f379f62d0dfc33a0e2f8771
Sachin <sachin28391@gmail.com>
2025-12-10 10:49:39 +0530

    Render deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     // API Gateway URL - routes to hotel and booking services
-    apiGatewayUrl: 'http://localhost:8000',
+    apiGatewayUrl: 'http://localhost:8080',
     hotelServiceUrl: 'http://localhost:8081', // Use gateway for hotel service
     bookingServiceUrl: 'http://localhost:8082' // Use gateway for booking service
 };
